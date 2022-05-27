6aa693aa8ada40f4930ea6c99c7bc7945a30c848
unknown <rafahmp123@gmail.com>
2022-05-27 18:07:50 -0300

    Criando script modal

--- 6-modal/app.js
+++ 6-modal/app.js
@@ -1,4 +1,22 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+const openBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+function modal() {
+  if(!modalOverlay.classList.contains('open-modal')) {
+    openBtn.addEventListener('click', () => {
+      modalOverlay.classList.add('open-modal');
+    })
+    closeBtn.addEventListener('click', () => {
+      modalOverlay.classList.remove('open-modal');
+    })
+  }
+  modalOverlay.addEventListener('click', (e) => {
+    console.log(e.target);
+    if(e.target.classList.contains('open-modal')) {
+      modalOverlay.classList.remove('open-modal');
+    }
+  })
+}
+
+modal();
