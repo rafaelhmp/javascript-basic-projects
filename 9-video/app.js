af142ad7051a184d4dd88ecf8e3c60d17000a7c7
unknown <rafahmp123@gmail.com>
2022-05-31 17:38:35 -0300

    Adicionando preloader ao script

--- 9-video/app.js
+++ 9-video/app.js
@@ -1,7 +1,3 @@
-// MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
-
 const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
 
@@ -13,4 +9,10 @@ btn.addEventListener('click', () => {
     btn.classList.remove('slide');
     video.play();
   }
+})
+
+// Preloader
+const preloader = document.querySelector('.preloader');
+window.addEventListener('load', () => {
+  preloader.classList.add('hide-preloader');
 })
