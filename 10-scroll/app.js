eb58cc230085c64ca00bda9c55bac96a436ef690
unknown <rafahmp123@gmail.com>
2022-06-01 19:00:35 -0300

    Adicionando script scroll

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -1,8 +1,3 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
 // ********** set date ************
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
@@ -37,8 +32,34 @@ window.addEventListener('scroll', () => {
     topLink.classList.add('show-link');
   } else {
     topLink.classList.remove('show-link');
-
   }
 })
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if(!fixedNav) {
+      position = position - navHeight;
+    }
+    if(navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+    linksContainer.style.height = 0;
+  })
+})
